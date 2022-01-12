6c9e34537ab609ca5cea7097079eca03f8002bb3
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-01-12 22:12:05 +0100

    Spritesheets

--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -6,14 +6,12 @@ import org.joml.Vector4f;
 import renderer.Texture;
 
 public class SpriteRenderer extends Component {
-
     private Vector4f color;
-
     private Sprite sprite;
 
     public SpriteRenderer(Vector4f color) {
         this.color = color;
-        this.sprite = new Sprite(null); //a sprite with null texture
+        this.sprite = new Sprite(null);
     }
 
     public SpriteRenderer(Sprite sprite) {
@@ -35,16 +33,10 @@ public class SpriteRenderer extends Component {
     }
 
     public Texture getTexture() {
-        return this.sprite.getTexture();
+        return sprite.getTexture();
     }
 
     public Vector2f[] getTexCoords() {
-        Vector2f[] texCoords = {
-                new Vector2f(1, 1),
-                new Vector2f(1, 0),
-                new Vector2f(0, 0),
-                new Vector2f(0, 1)
-        };
-        return texCoords;
+        return sprite.getTexCoords();
     }
 }
--- src/main/java/util/AssetPool.java
+++ src/main/java/util/AssetPool.java
@@ -1,5 +1,6 @@
 package util;
 
+import components.Spritesheet;
 import renderer.Shader;
 import renderer.Texture;
 
@@ -11,6 +12,7 @@ public class AssetPool {
 
     private static Map<String, Shader>  shaders = new HashMap<>();
     private static Map<String, Texture>  textures = new HashMap<>();
+    private static Map<String, Spritesheet> spriteSheets = new HashMap<>();
 
     public static  Shader getShader(String resourceName){
         File file = new File(resourceName);
@@ -33,6 +35,25 @@ public class AssetPool {
             AssetPool.textures.put(file.getAbsolutePath(), texture);
             return texture;
         }
+    }
+
+    public static void addSpritesheet(String resourceName, Spritesheet spritesheet){
+        File file = new File(resourceName);
+        if(!AssetPool.spriteSheets.containsKey(file.getAbsolutePath())){
+            //Se ancora non c'è tra gli spritesheets
+            AssetPool.spriteSheets.put(file.getAbsolutePath(),spritesheet);
+        }
+    }
+
+    public static Spritesheet getSpritesheet(String resourceName) {
+        File file = new File(resourceName);
+        if (!AssetPool.spriteSheets.containsKey(file.getAbsolutePath())) {
+            //Se ancora non c'è tra gli spritesheets ERRORE!!!
+            assert false : "Error: Tried to access spritesheet '"+resourceName+
+                    "' and it has not been added to asset pool.";
+            }
+        return AssetPool.spriteSheets.getOrDefault(file.getAbsolutePath(),null); //null come default
+
     }
 
 }
